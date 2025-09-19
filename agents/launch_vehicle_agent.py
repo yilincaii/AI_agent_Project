4a37151f27ce079d633942acee1aeebaaa44f379
nayuanju <yic153@ucsd.edu>
2025-09-18 22:59:27 -0700

    feat: add Flask app entry point (app.py) and fix running error for launch_vehicle_agent.py

--- agents/launch_vehicle_agent.py
+++ agents/launch_vehicle_agent.py
@@ -5,12 +5,12 @@ def create_launch_vehicle_agent(llm):
     return create_react_agent(
         model =llm,
         tools = [get_launches],
-        prompt = {
+        prompt = (
              "You are a launch vehicle agent.\n\n"
             "INSTRUCTIONS:\n"
             "- Assist only with space rocket launch vehicle related tasks.\n"
             "- Make sure you use the tools that you have provided to you.\n"
             "- Once you have the launch vehicle information, respond with the details and then indicate you are transferring back to the supervisor with the 'FINAL ANSWER'."
-        }
+        ),
         name = "launch_vehicle_agent",
     )
